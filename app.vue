703fb60a2955f419ee0c1c6385159f7999c40266
ankkaya <staryou@126.com>
2024-03-06 01:59:35 +0800

    环境变量（学习中）

--- app.vue
+++ app.vue
@@ -3,3 +3,12 @@
     <NuxtWelcome />
   </div>
 </template>
+
+<script setup lang="ts">
+const runtimeConfig = useRuntimeConfig()
+
+console.log(runtimeConfig.secretKey)
+console.log(runtimeConfig.public)
+
+console.log(process.env)
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  runtimeConfig: {
+    secretKey: "my-secret-key",
+    public: {
+      baseURL: "/api",
+    },
+  },
 })
